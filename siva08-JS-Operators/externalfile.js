833aa9be393a017056b6c5fa45ffdc5de691b51c
siva1855 <siva.muluguri@gmail.com>
2021-04-19 17:36:19 +0530

    test

---
+++ siva08-JS-Operators/externalfile.js
@@ -0,0 +1,9 @@
+//Arithmetic operator
+var arthValue1=10;
+ var arthValue2=20;
+ var ArithmeticOperator=arthValue1+arthValue2;
+
+//Assigment Operator
+var AssigValue1 = 10;
+AssigValue1 = AssigValue1 + 13;
+
